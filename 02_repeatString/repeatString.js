67d20ab14b9c2eb4f87c3e7f5a682099942c2df4
Anton Zabulonov <zabulonov.a@gmail.com>
2022-04-19 22:52:18 +0300

    Complete exercises 1 trough 3

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,11 @@
-const repeatString = function() {
-
+const repeatString = function (str, num) {
+	if (num < 0) return 'ERROR';
+	let arr = [];
+	for (i = 0; i < num; i++) {
+		arr.push(str);
+	}
+	return arr.join('');
 };
-
+repeatString('hey', 3);
 // Do not edit below this line
 module.exports = repeatString;
