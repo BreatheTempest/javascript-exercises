5656c62a9371e14336f1133ed59eeda7f89ad7a8
Anton Zabulonov <zabulonov.a@gmail.com>
2022-04-20 12:58:53 +0300

    Complete fourth js exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,9 @@
-const removeFromArray = function() {
-
+const removeFromArray = function (arr, ...args) {
+	const newArr = [];
+	for (let i = 0; i < arr.length; i++) {
+		if (args.indexOf(arr[i]) === -1) newArr.push(arr[i]);
+	}
+	return newArr;
 };
 
 // Do not edit below this line
