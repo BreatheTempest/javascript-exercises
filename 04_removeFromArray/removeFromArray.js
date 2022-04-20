031bb24ddde7623b14146f16e87037d1aa3fe93f
Anton Zabulonov <zabulonov.a@gmail.com>
2022-04-20 13:01:32 +0300

    Reformat answer to fourth exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,9 +1,10 @@
 const removeFromArray = function (arr, ...args) {
-	const newArr = [];
-	for (let i = 0; i < arr.length; i++) {
-		if (args.indexOf(arr[i]) === -1) newArr.push(arr[i]);
-	}
-	return newArr;
+	// const newArr = [];
+	// for (let i = 0; i < arr.length; i++) {
+	// 	if (args.indexOf(arr[i]) === -1) newArr.push(arr[i]);
+	// }
+	// return newArr;
+	return arr.filter((item) => args.indexOf(item) === -1);
 };
 
 // Do not edit below this line
