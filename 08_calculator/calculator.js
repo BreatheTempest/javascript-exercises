1e2a42f90046444b5a8a99f3e6d0f7975004cf22
Anton Zabulonov <zabulonov.a@gmail.com>
2022-04-29 19:42:29 +0300

    Complete eighth exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,33 +1,45 @@
-const add = function() {
-	
+const add = function (num1, num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function (num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function (arr) {
+	let total = 0;
+	for (let num of arr) {
+		total += num;
+	}
+	return total;
 };
 
-const multiply = function() {
-
+const multiply = function (arr) {
+	let total = 1;
+	for (let num of arr) {
+		total *= num;
+	}
+	return total;
 };
 
-const power = function() {
-	
+const power = function (num1, num2) {
+	let total = num1;
+	for (let i = 1; i < num2; i++) total *= num1;
+	return total;
 };
 
-const factorial = function() {
-	
+const factorial = function (num) {
+	let total = 1;
+	for (let i = 1; i <= num; i++) total *= i;
+	return total;
 };
 
 // Do not edit below this line
 module.exports = {
-  add,
-  subtract,
-  sum,
-  multiply,
-  power,
-  factorial
+	add,
+	subtract,
+	sum,
+	multiply,
+	power,
+	factorial,
 };
