d8c2a93bfc4e963c74cf1ce403072471638c5892
Anton Zabulonov <zabulonov.a@gmail.com>
2022-04-30 14:04:14 +0300

    Complete tenth exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,13 @@
-const fibonacci = function() {
-
+const fibonacci = function (num) {
+	let arr = [1, 1];
+	if (num < 0) return 'OOPS';
+	if (num > 2) {
+		for (let i = 2; i < num; i++) {
+			let temp = arr[arr.length - 1] + arr[arr.length - 2];
+			arr.push(temp);
+		}
+	}
+	return arr[num - 1];
 };
-
 // Do not edit below this line
 module.exports = fibonacci;
