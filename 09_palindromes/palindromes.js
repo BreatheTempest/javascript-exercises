557c96d0309da993427b4181acad6287644ac38c
Anton Zabulonov <zabulonov.a@gmail.com>
2022-04-30 13:06:04 +0300

    Complete ninth exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,12 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+	str = str.replace(/[\W\d]/g, '').toLowerCase();
+	let firstHalf = str.slice(
+		0,
+		str.length % 2 === 0 ? str.length / 2 : str.length / 2 + 1
+	);
+	let secondHalf = str.slice(str.length / 2);
+	return secondHalf.split('').reverse().join('') === firstHalf;
 };
-
+console.log(palindromes('Animal loots foliated detail of stool lamina.'));
 // Do not edit below this line
 module.exports = palindromes;
